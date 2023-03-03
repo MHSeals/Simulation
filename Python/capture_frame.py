d35868d8b033e1644cc965429e70c74943ecccfc
samminhch <45446622+samminhch@users.noreply.github.com>
2023-03-03 16:02:23 -0600

    Draws bounding boxes around buoys in simulation

--- Python/capture_frame.py
+++ Python/capture_frame.py
@@ -2,7 +2,6 @@ from gstream import Video
 import time
 import cv2
 
-
 def main():
     video = Video()
     while not video.frame_available():
@@ -10,6 +9,8 @@ def main():
 
     frame = video.frame()
     cv2.imwrite('./frames/frame-1.jpg', frame)
+    cv2.imshow(frame)
+    cv2.destroyAllWindows()
     pass
 
 
--- Python/detect_buoys.py
+++ Python/detect_buoys.py
@@ -1,72 +1,57 @@
 import cv2
 import numpy as np
 import time
+import colorsys
 from gstream import Video
 
-capture = Video()
-try:
-    while True:
-        if not capture.frame_available():
-            continue
+def draw_box(frame, mask, boxName):
+    contours, _ = cv2.findContours(mask,
+                                   cv2.RETR_EXTERNAL,
+                                   cv2.CHAIN_APPROX_SIMPLE)
 
-        # Take each frame
-        frame = capture.frame()
+    contour = sorted(contours, key=lambda c: cv2.contourArea(c), reverse=True)[0]
+    x, y, w, h = cv2.boundingRect(contour)
+    imageFrame = cv2.rectangle(frame, (x, y), 
+                               (x + w, y + h), 
+                               (0, 0, 255), 2)
+      
+    cv2.putText(frame, boxName, (x, y),
+                cv2.FONT_HERSHEY_SIMPLEX, 1.0,
+                (0, 0, 255))
 
-        # Convert BGR to HSV
-        hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
-        # filter red
-        lower_red   = np.array([ 0,  20,  20])
-        upper_red   = np.array([ 0, 255, 255])
-        red_mask = cv2.inRange(hsv, lower_red, upper_red)
+def process_frame(frame):
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
-        # filter green
-        lower_green = np.array([50,  20,  20])
-        upper_green = np.array([50, 255, 255])
-        green_mask = cv2.inRange(hsv, lower_green, upper_green)
+    # filter red
+    lower_red = np.array([ 0, 50, 50])
+    upper_red = np.array([10,255,255])
+    red_mask    = cv2.inRange(hsv, lower_red, upper_red)
 
-        kernel = np.ones((5, 5), "uint8")
+    # filter green
+    lower_green = np.array([36,0,0])
+    upper_green = np.array([86,255,255])
+    green_mask  = cv2.inRange(hsv, lower_green, upper_green)
 
-        red_mask = cv2.dilate(red_mask, kernel)
-        res_red  = cv2.bitwise_and(frame, frame, mask = red_mask)
+    cv2.imshow("Red Mask", red_mask)
+    cv2.imshow("Green Mask", green_mask)
 
-        green_mask = cv2.dilate(green_mask, kernel)
-        res_green  = cv2.bitwise_and(frame, frame, mask = green_mask)
+    draw_box(frame, red_mask, "Red Buoy")
+    draw_box(frame, green_mask, "Green Buoy")
 
-        contours, hierarchy = cv2.findContours(red_mask,
-                                                cv2.RETR_TREE,
-                                                cv2.CHAIN_APPROX_SIMPLE)
 
-        for pic, contour in enumerate(contours):
-            area = cv2.contourArea(contour)
-            if area > 300:
-                x, y, w, h = cv2.boundingRect(contour)
-                imageFrame = cv2.rectangle(frame, (x, y), 
-                                           (x + w, y + h), 
-                                           (0, 0, 255), 2)
-                  
-                cv2.putText(frame, "Red Buoy", (x, y),
-                            cv2.FONT_HERSHEY_SIMPLEX, 1.0,
-                            (0, 0, 255))
-        
-        contours, hierarchy = cv2.findContours(green_mask,
-                                                cv2.RETR_TREE,
-                                                cv2.CHAIN_APPROX_SIMPLE)
+# Create the video object
+# Add port= if is necessary to use a different one
+video = Video()
 
-        for pic, contour in enumerate(contours):
-            area = cv2.contourArea(contour)
-            if area > 300:
-                x, y, w, h = cv2.boundingRect(contour)
-                imageFrame = cv2.rectangle(frame, (x, y), 
-                                           (x + w, y + h), 
-                                           (0, 0, 255), 2)
-                  
-                cv2.putText(frame, "Green Buoy", (x, y),
-                            cv2.FONT_HERSHEY_SIMPLEX, 1.0,
-                            (0, 0, 255))
+while True:
+    # Wait for the next frame
+    if not video.frame_available():
+        continue
 
-        cv2.imshow("Buoy Detection", frame)
-        time.sleep(.5)
-except KeyboardInterrupt as err:
-    cv2.destroyAllWindows()
-
+    frame = video.frame()
+    process_frame(frame)
+    cv2.imshow("Raw Image", frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+    time.sleep(0.25)
